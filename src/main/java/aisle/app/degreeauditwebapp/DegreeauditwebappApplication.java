ea787a6c352e307b6a35b4167830899c98fa7f08
PakshP <pakshpatel1@gmail.com>
2024-05-06 13:55:43 -0400

    init

---
+++ src/main/java/aisle/app/degreeauditwebapp/DegreeauditwebappApplication.java
@@ -0,0 +1,13 @@
+package aisle.app.degreeauditwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreeauditwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegreeauditwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aisle/app/degreeauditwebapp/DegreeauditwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package aisle.app.degreeauditwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegreeauditwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
